1886c68e2f356ec23c44dd21feca3444bbf66690
Christopher Bussick <christopher.bussick@web.de>
2021-10-11 21:26:29 +0200

    Add code-template-tool files

---
+++ .vscode/templates/CBFunctionComponentWithoutViewModel/___componentName___/___componentName___Styles.ts
@@ -0,0 +1,9 @@
+import { createStyles, makeStyles, Theme } from "@material-ui/core";
+
+export const use___componentName___Styles = makeStyles((theme: Theme) =>
+  createStyles({
+    wrapper: {
+      backgroundColor: theme.palette.primary.main,
+    },
+  })
+);
