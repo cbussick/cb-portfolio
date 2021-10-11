f11210a99423ad90c487def0f8b3d661124865c2
Christopher Bussick <christopher.bussick@web.de>
2021-10-11 23:37:21 +0200

    Add shouldForwardProp to example

--- src/CBHeader/CBHeaderStyles.ts
+++ src/CBHeader/CBHeaderStyles.ts
@@ -1,9 +1,12 @@
 import { styled, Typography } from "@mui/material";
 import { StyledHeaderProps } from "./CBHeaderInterfaces";
 
-export const StyledHeader = styled(Typography)<StyledHeaderProps>(
-  ({ theme, ...props }) => ({
-    backgroundColor: theme.palette.secondary.main,
-    color: props.testColor,
-  })
-);
+const shouldForwardProp = (prop: keyof StyledHeaderProps) =>
+  prop !== "testColor";
+
+export const StyledHeader = styled(Typography, {
+  shouldForwardProp,
+})<StyledHeaderProps>(({ theme, ...props }) => ({
+  backgroundColor: theme.palette.secondary.main,
+  color: props.testColor,
+}));
