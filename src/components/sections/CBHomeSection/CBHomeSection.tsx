2ec032fc85a9b99d813241f3611ef12401811f2f
Christopher Bussick <christopher.bussick@web.de>
2021-10-18 23:42:49 +0200

    Add sample text

--- src/components/sections/CBHomeSection/CBHomeSection.tsx
+++ src/components/sections/CBHomeSection/CBHomeSection.tsx
@@ -1,4 +1,4 @@
-import { Typography } from "@mui/material";
+import { Divider, Link, Typography } from "@mui/material";
 import React from "react";
 import { Section } from "../../../interfaces/Section";
 import CBSectionContainer from "../../CBSectionContainer/CBSectionContainer";
@@ -10,6 +10,18 @@ const CBHomeSection = (): JSX.Element => (
       👷🚧
       <br />
       Work in <CBTextGradient variant="h1">progress</CBTextGradient>! :)
+      <Divider sx={{ marginTop: 5 }} />
+      <br />
+      <CBTextGradient variant="h1">Welcome</CBTextGradient> 👋
+      <br />
+      <Typography variant="h3">
+        I&apos;m Christopher
+        <br />I live in{" "}
+        <Link href="https://en.wikipedia.org/wiki/Gladbeck" target="_blank">
+          Gladbeck
+        </Link>
+        , Germany 🇩🇪.
+      </Typography>
     </Typography>
   </CBSectionContainer>
 );
