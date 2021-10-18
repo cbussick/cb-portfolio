74386ee60fd58a0cd9ca70301d51eaf58e2c7333
Christopher Bussick <43065398+ChristopherBussick@users.noreply.github.com>
2021-10-18 20:57:26 +0200

    Restructure folders (#12)
    * Clean up
    
    * Add extra components folder
    
    * Add sections folder

--- src/components/sections/CBHomeSection/CBHomeSection.tsx
+++ src/components/sections/CBHomeSection/CBHomeSection.tsx
@@ -1,7 +1,7 @@
 import { Typography } from "@mui/material";
 import React from "react";
-import CBSectionContainer from "../CBSectionContainer/CBSectionContainer";
-import { Section } from "../interfaces/Section";
+import { Section } from "../../../interfaces/Section";
+import CBSectionContainer from "../../CBSectionContainer/CBSectionContainer";
 
 const CBHomeSection = (): JSX.Element => (
   <CBSectionContainer section={Section.Home}>
--- src/components/sections/CBProjectsSection/CBProjectsSection.tsx
+++ src/components/sections/CBProjectsSection/CBProjectsSection.tsx
@@ -1,7 +1,7 @@
 import { Typography } from "@mui/material";
 import React from "react";
-import CBSectionContainer from "../CBSectionContainer/CBSectionContainer";
-import { Section } from "../interfaces/Section";
+import { Section } from "../../../interfaces/Section";
+import CBSectionContainer from "../../CBSectionContainer/CBSectionContainer";
 
 const CBProjectsSection = (): JSX.Element => (
   <CBSectionContainer section={Section.Projects}>
