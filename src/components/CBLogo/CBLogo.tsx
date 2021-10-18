491505279c3344240ef5764f2f8d0d1b52d0bc34
Christopher Bussick <christopher.bussick@web.de>
2021-10-18 23:51:39 +0200

    Clean up and fix link styling

--- src/components/CBLogo/CBLogo.tsx
+++ src/components/CBLogo/CBLogo.tsx
@@ -1,17 +1,19 @@
-import { Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import React from "react";
 import CBTextGradient from "../CBTextGradient/CBTextGradient";
 import { StyledLogo } from "./CBLogoStyles";
 
 const CBLogo = (): JSX.Element => (
-  // Disabled, because `href="#"` seems like an alright href to me here,
-  // which takes you up to the beginning of the page.
-  // eslint-disable-next-line jsx-a11y/anchor-is-valid
-  <StyledLogo href="#">
-    <Typography variant="h5" sx={{ fontWeight: 600 }}>
-      👋 <CBTextGradient variant="h5">Christopher Bussick</CBTextGradient>
-    </Typography>
-  </StyledLogo>
+  <Box sx={{ flexGrow: 1, display: "flex" }}>
+    {/* Disabled, because `href="#"` seems like an alright href to me here,
+    which takes you up to the beginning of the page. 
+    // eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+    <StyledLogo href="#">
+      <Typography variant="h5" sx={{ fontWeight: 600 }}>
+        👋 <CBTextGradient variant="h5">Christopher Bussick</CBTextGradient>
+      </Typography>
+    </StyledLogo>
+  </Box>
 );
 
 export default CBLogo;
