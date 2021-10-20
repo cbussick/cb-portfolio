516f9a0a44d59459a1767d8b435ae2c818561c32
Christopher Bussick <43065398+ChristopherBussick@users.noreply.github.com>
2021-10-20 21:58:50 +0200

    Project cards (#17)
    * Clean up
    
    * Rename picture
    
    * Add first draft of project cards
    
    * Add video to cards
    
    * Style projects section
    
    * Fix overlapping when sections overflow

---
+++ src/components/sections/CBProjectsSection/projectsData.tsx
@@ -0,0 +1,58 @@
+import { Link } from "@mui/material";
+import React from "react";
+import { Project, ProjectThumbnailType } from "../../../interfaces/Project";
+import bpmTapper from "../../../resources/bpm-tapper-thumbnail.png";
+import portfolio from "../../../resources/portfolio-website.png";
+
+export const projects: Project[] = [
+  {
+    id: "virtual-boxing-simulator",
+    name: "Virtual Boxing Simulator",
+    thumbnailType: ProjectThumbnailType.Video,
+    url: "https://www.youtube.com/watch?v=3zIW1LwYYDQ",
+    description: (
+      <>
+        A game simulating the sport of boxing in virtual reality. This project
+        was made by our group of 6 students from the{" "}
+        <Link href="https://www.w-hs.de/" target="_blank">
+          WHS
+        </Link>{" "}
+        as part of our respective curricula. It features a rhythm-based game
+        mode with multiple difficulties, progress recording and character
+        customization.
+      </>
+    ),
+  },
+  {
+    id: "bpm-tapper",
+    name: "BPM Tapper",
+    thumbnailType: ProjectThumbnailType.Image,
+    thumbnail: bpmTapper,
+    url: "https://christopherbussick.github.io/bpm-tapper/",
+    description: (
+      <>
+        A bpm tapper is a tool used in music production to figure out the tempo
+        of a song, measured in beats per minute (bpm). The tool is usually part
+        of a{" "}
+        <Link
+          href="https://en.wikipedia.org/wiki/Digital_audio_workstation"
+          target="_blank"
+        >
+          DAW
+        </Link>
+        , installed on a computer. This project is meant to make the tool
+        browser-based.
+      </>
+    ),
+  },
+  {
+    id: "portfolio-website",
+    name: "Portfolio Website",
+    thumbnailType: ProjectThumbnailType.Image,
+    thumbnail: portfolio,
+    url: "https://www.christopherbussick.com/",
+    description: (
+      <>The website you are currently on! It serves as my digital portfolio.</>
+    ),
+  },
+];
---
+++ src/interfaces/Project.ts
@@ -0,0 +1,16 @@
+import { TypographyProps } from "@mui/material";
+
+export enum ProjectThumbnailType {
+  Image = "image",
+  Video = "video",
+  None = "none",
+}
+
+export interface Project {
+  id: string;
+  name: string;
+  thumbnailType: ProjectThumbnailType;
+  thumbnail?: string;
+  url: string;
+  description: TypographyProps["children"];
+}
