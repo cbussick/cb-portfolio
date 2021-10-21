1f27e1697af6a21789c067d9cef175b973da9717
Christopher Bussick <43065398+ChristopherBussick@users.noreply.github.com>
2021-10-21 23:37:46 +0200

    Fill contact section (#22)
    * Clean up
    
    * Change card title gradient styling, increase card width
    
    * Add tech stack to projects
    
    * Slight restructuring of home section
    
    * Add comment
    
    * Remove smiley
    
    * Add contact information

--- src/components/sections/CBContactSection/CBContactSection.tsx
+++ src/components/sections/CBContactSection/CBContactSection.tsx
@@ -1,16 +1,21 @@
-import { Typography } from "@mui/material";
+import { Link, Typography } from "@mui/material";
 import React from "react";
 import { Section } from "../../../interfaces/Section";
 import CBSectionContainer from "../../CBSectionContainer/CBSectionContainer";
 import CBTextGradient from "../../CBTextGradient/CBTextGradient";
+
+const eMail = "bussick.christopher@gmail.com";
 
 const CBContactSection = (): JSX.Element => (
   <CBSectionContainer section={Section.Contact}>
     <Typography variant="h1" component="div" sx={{ textAlign: "center" }}>
       👋
       <br />
-      <CBTextGradient variant="h1">Reach out</CBTextGradient> to me! :)
+      <CBTextGradient variant="h1">Reach out</CBTextGradient> to me!
     </Typography>
+    <address>
+      EMail: <Link href={`mailto:${eMail}`}>{eMail}</Link>
+    </address>
   </CBSectionContainer>
 );
 
