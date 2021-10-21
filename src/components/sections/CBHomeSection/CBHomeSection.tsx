6189e67ac9205acc27b5d6c6ebc67eaeff2ad008
Christopher Bussick <43065398+ChristopherBussick@users.noreply.github.com>
2021-10-21 23:10:15 +0200

    Adjust sections (#21)
    * Clean up
    
    * Change card title gradient styling, increase card width
    
    * Add tech stack to projects
    
    * Slight restructuring of home section
    
    * Fix validate DOM nesting error
    
    * Standartize h1 margin
    
    * Change text justification

--- src/components/sections/CBHomeSection/CBHomeSection.tsx
+++ src/components/sections/CBHomeSection/CBHomeSection.tsx
@@ -9,21 +9,15 @@ const CBHomeSection = (): JSX.Element => {
   const theme = useTheme();
 
   return (
-    <CBSectionContainer section={Section.Home} sx={{ textAlign: "center" }}>
-      <Typography
-        variant="h1"
-        component="div"
-        sx={{
-          marginBottom: theme.spacing(5),
-        }}
-      >
+    <CBSectionContainer section={Section.Home}>
+      <Typography variant="h1" component="div" sx={{ textAlign: "center" }}>
         👋
         <br />
         <CBTextGradient variant="h1">Welcome</CBTextGradient>
       </Typography>
-      <Typography>
-        I&apos;m Christopher! :)
-        <br /> I work in software development for both the web and{" "}
+      <Typography sx={{ textAlign: "justify" }}>
+        I&apos;m Christopher!
+        <br />I work in software development for both the web and{" "}
         <Link
           href="https://en.wikipedia.org/wiki/Virtual_reality"
           target="_blank"
@@ -41,30 +35,30 @@ const CBHomeSection = (): JSX.Element => {
         in Gelsenkirchen, Germany 🇩🇪. Apart from computer science stuff I also
         enjoy anime/manga, sports and music production. I currently live in
         Gladbeck.
-        <figure
-          style={{
-            padding: 0,
-            margin: 0,
-            marginTop: theme.spacing(7),
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-          }}
-        >
-          <img
-            src={meAndBruno}
-            alt="It's me! :)"
-            style={{
-              width: 650,
-              maxWidth: "100%",
-              boxShadow: theme.shadows[10],
-            }}
-          />
-          <figcaption style={{ textAlign: "start" }}>
-            <Typography variant="caption">Me and Bruno :)</Typography>
-          </figcaption>
-        </figure>
       </Typography>
+      <figure
+        style={{
+          padding: 0,
+          margin: 0,
+          marginTop: theme.spacing(7),
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <img
+          src={meAndBruno}
+          alt="It's me! :)"
+          style={{
+            width: 650,
+            maxWidth: "100%",
+            boxShadow: theme.shadows[10],
+          }}
+        />
+        <figcaption style={{ textAlign: "start" }}>
+          <Typography variant="caption">Me and Bruno :)</Typography>
+        </figcaption>
+      </figure>
     </CBSectionContainer>
   );
 };
