1e9cf28ddca41ae3bdb80c52ec9ea180ff9e5f60
Christopher Bussick <christopher.bussick@web.de>
2021-10-23 01:24:16 +0200

    Add scrolling transition

--- src/components/CBHeader/CBHeader.tsx
+++ src/components/CBHeader/CBHeader.tsx
@@ -1,24 +1,30 @@
 import { GitHub } from "@mui/icons-material";
 import { Box, IconButton, Toolbar, Typography } from "@mui/material";
 import React from "react";
+import { scrollToElement } from "../../helpers/scrollToElement";
 import CBLogo from "../CBLogo/CBLogo";
 import { CBHeaderProps } from "./CBHeaderInterfaces";
 import { StyledHeader, StyledHeaderLink } from "./CBHeaderStyles";
 import { headerLinks } from "./headerLinkData";
 
 const CBHeader = (props: CBHeaderProps): JSX.Element => {
-  const headerElements: JSX.Element[] = headerLinks.map((link) => (
-    <StyledHeaderLink
-      key={link.id}
-      href={`#${link.id}`}
-      startIcon={<link.icon />}
-      classes={{ startIcon: "link-icon" }}
-    >
-      <Typography variant="subtitle1" component="span" className="link-label">
-        {link.label}
-      </Typography>
-    </StyledHeaderLink>
-  ));
+  const headerElements: JSX.Element[] = headerLinks.map((link) => {
+    const selector = link.id === "" ? undefined : `#${link.id}`;
+
+    return (
+      <StyledHeaderLink
+        key={link.id}
+        href={selector}
+        onClick={(e) => scrollToElement(e, selector)}
+        startIcon={<link.icon />}
+        classes={{ startIcon: "link-icon" }}
+      >
+        <Typography variant="subtitle1" component="span" className="link-label">
+          {link.label}
+        </Typography>
+      </StyledHeaderLink>
+    );
+  });
 
   return (
     <StyledHeader isPageScrolled={props.isPageScrolled}>
