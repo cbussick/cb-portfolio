fffba77edc68f3fb7d9fc2228196c512c7205bb9
Christopher Bussick <bussick.christopher@gmail.com>
2022-10-02 14:06:20 +0200

    Adjust templates

--- .vscode/templates/CBFunctionComponent/___componentName___/___componentName___.tsx
+++ .vscode/templates/CBFunctionComponent/___componentName___/___componentName___.tsx
@@ -1,15 +1,11 @@
-import { Box, useTheme } from "@mui/material";
+import { Box } from "@mui/material";
 import { ___Component_Name___Props } from "./___componentName___Interfaces";
 import { ___Component_Name___Styles } from "./___componentName___Styles";
 
 function ___Component_Name___(props: ___Component_Name___Props): JSX.Element {
-  const theme = useTheme();
+  const styles = ___Component_Name___Styles();
 
-  return (
-    <Box sx={___Component_Name___Styles(theme)}>
-      Hey ___Component_Name___ 🚀
-    </Box>
-  );
+  return <Box sx={styles.wrapper}>Hey ___Component_Name___ 🚀</Box>;
 }
 
 export default ___Component_Name___;
