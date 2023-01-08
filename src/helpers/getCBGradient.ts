cef17a9d1ffaf956269473b8802c4e77a320201f
Christopher Bussick <bussick.christopher@gmail.com>
2023-01-08 10:31:50 +0100

    Allow numbers for gradient degrees

--- src/helpers/getCBGradient.ts
+++ src/helpers/getCBGradient.ts
@@ -1,22 +1,27 @@
 import theme from "../theme/theme";
 
 export enum CBGradientDirection {
-  TopToBottom = "top-to-bottom",
-  LeftToRight = "left-to-right",
   BottomToTop = "bottom-to-top",
+  LeftToRight = "left-to-right",
+  TopToBottom = "top-to-bottom",
   RightToLeft = "right-to-left",
 }
 
 const gradientTypeMap: Record<CBGradientDirection, number> = {
-  [CBGradientDirection.TopToBottom]: 180,
-  [CBGradientDirection.LeftToRight]: 90,
   [CBGradientDirection.BottomToTop]: 0,
+  [CBGradientDirection.LeftToRight]: 90,
+  [CBGradientDirection.TopToBottom]: 180,
   [CBGradientDirection.RightToLeft]: 270,
 };
 
-export const getCBGradient = (direction?: CBGradientDirection) =>
-  `linear-gradient(${
-    direction
-      ? gradientTypeMap[direction]
-      : gradientTypeMap[CBGradientDirection.LeftToRight]
-  }deg, ${theme.palette.primary.main} 30%, ${theme.palette.primary.dark} 100%)`;
+export const getCBGradient = (direction?: CBGradientDirection | number) => {
+  let degrees = gradientTypeMap[CBGradientDirection.LeftToRight];
+  if (direction) {
+    if (typeof direction === "number") {
+      degrees = direction;
+    } else {
+      degrees = gradientTypeMap[direction];
+    }
+  }
+  return `linear-gradient(${degrees}deg, ${theme.palette.primary.main} 30%, ${theme.palette.primary.dark} 100%)`;
+};
