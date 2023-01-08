303c166f6ef689113b0984f7529301baf400eb3c
Christopher Bussick <bussick.christopher@gmail.com>
2023-01-08 11:15:41 +0100

    Add documentation

--- src/helpers/getBorderGradient.ts
+++ src/helpers/getBorderGradient.ts
@@ -1,5 +1,13 @@
 import { CSSProperties } from "react";
 
+/**
+ * Allows using a gradient inside a border, even if the object has a `border-radius`.
+ *
+ * @param objectBackgroundColor of the object which should have the border.
+ * @param gradient to apply to the border.
+ * @param borderWidth of the border.
+ * @returns the CSS properties necessary for having a gradient inside a border.
+ */
 export const getBorderGradient = (
   objectBackgroundColor: string,
   gradient: string,
