6bb38d1cd58ef0917ba6ce327c18e4cff2664ff7
Christopher Bussick <bussick.christopher@gmail.com>
2023-01-08 10:48:01 +0100

    Reduce padding of cards on mobile responsible for overflow

--- src/components/CBCVCard/CBCVCardStyles.ts
+++ src/components/CBCVCard/CBCVCardStyles.ts
@@ -3,7 +3,7 @@ import { makeSx } from "../../helpers/makeSx";
 export const useCBCVCardStyles = () =>
   makeSx({
     card: (theme) => ({
-      p: theme.spacing(7, 6),
+      p: { xs: theme.spacing(7, 3), md: theme.spacing(7, 6) },
     }),
     cardHeader: {
       p: 0,
